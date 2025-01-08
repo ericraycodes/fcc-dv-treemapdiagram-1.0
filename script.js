b91123ceebba4df01bbb39859325020bc1d29235
lasernoy <cloudy.nny07@gmail.com>
2025-01-08 22:12:23 +0800

    added the urls for the external dataset in the data object.

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
 // TREE MAP DATA
 const repository = "https://github.com/ericraycodes/fcc-dv-treemapdiagram-1.0.git/"
 const DATA = {
-	"navigator" : {
-		"element" : "nav",
-		"id" : "navigator",
-		"items" : {
-			"element" : "a",
-			"class" : "nav-links",
-		}
+	// URLs of data to fetch
+	"fetch" : {
+		// kickstarter pledges
+		"url1" : "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json",
+		// movie sales
+		"url2" : "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/movie-data.json",
+		// video game sales
+		"url3" : "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json"
 	},
-	"heading" : {
-		"container" : "header",
-		"title" : {
-			"text" : null,
-			"id" : "title",
-			"element" : "h1"
-		},
-		"description" : {
-			"text" : null,
-			"id" : "description",
-			"element" : "h2"
-		}
-	},
-	"footing" : {
-		"container" : "footer",
-		"html" : `<p>This <a href="${repository}" target="_blank">project</a> is made for freeCodeCamp certification purposes. Eric Ray Saladar. 2025.</p>`
-	},
+	// storage for fetched dataset
+	"dataset" : null,
 
 	// incomplete, needs the d3 aspects for drawing the diagram
 	"diagram" : {
@@ -62,5 +48,31 @@ const DATA = {
 			"html" : null,
 			"propVal" : "data-value"
 		}
-	}
+	},
+
+	"navigator" : {
+		"element" : "nav",
+		"id" : "navigator",
+		"items" : {
+			"element" : "a",
+			"class" : "nav-links",
+		}
+	},
+	"heading" : {
+		"container" : "header",
+		"title" : {
+			"text" : null,
+			"id" : "title",
+			"element" : "h1"
+		},
+		"description" : {
+			"text" : null,
+			"id" : "description",
+			"element" : "h2"
+		}
+	},
+	"footing" : {
+		"container" : "footer",
+		"html" : `<p>This <a href="${repository}" target="_blank">project</a> is made for freeCodeCamp certification purposes. Eric Ray Saladar. 2025.</p>`
+	},
 };
