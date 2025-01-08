01fdd9fcd0ec38ca32d64659deee679a90697a42
lasernoy <cloudy.nny07@gmail.com>
2025-01-08 14:20:15 +0800

    diagram data

---
+++ script.js
@@ -0,0 +1,65 @@
+// TREE MAP DATA
+const repository = "https://github.com/ericraycodes/fcc-dv-treemapdiagram-1.0.git/"
+const DATA = {
+	"navigator" : {
+		"element" : "nav",
+		"id" : "navigator",
+		"items" : {
+			"element" : "a",
+			"class" : "nav-links",
+		}
+	},
+	"heading" : {
+		"container" : "header",
+		"title" : {
+			"text" : null,
+			"id" : "title",
+			"element" : "h1"
+		},
+		"description" : {
+			"text" : null,
+			"id" : "description",
+			"element" : "h2"
+		}
+	},
+	"footing" : {
+		"container" : "footer",
+		"html" : `<p>This <a href="${repository}" target="_blank">project</a> is made for freeCodeCamp certification purposes. Eric Ray Saladar. 2025.</p>`
+	},
+
+	"diagram" : {
+		"container" : "main",
+		"id" : "diagram",
+		"dimension" : {
+			"height" : null,
+			"width" : null,
+			"pad" : null,
+		},
+		"tree" : {
+			"element" : "svg",
+			"tiles" : {
+				"element" : "rect",
+				"class" : "tile",
+				"colors" : null,	// at least 2 color fills
+				"propName" : "data-name",
+				"propCat" : "data-category",
+				"propVal" : "data-value",
+			}
+		},
+		"legend" : {
+			"element" : "g",
+			"id" : "legend",
+			"items" : {
+				"element" : "rect",
+				"class" : "legend-items",
+				"colors" : null,	// at least 2 color fills
+			}
+		},
+		"tooltip" : {
+			"element" : "div",
+			"id" : "tooltip"
+			"html" : null,
+			"propVal" : "data-value"
+		}
+	}
+};
